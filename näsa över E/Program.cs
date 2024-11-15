236600679eec98ba8e613bbb86d3fcb96ea04d98
David Öjebrink Olssson <David.ojebrinkolsson@elev.ga.ntig.se>
2024-11-15 13:29:12 +0100

    klar

--- näsa över E/Program.cs
+++ näsa över E/Program.cs
@@ -6,60 +6,89 @@
 // }
 
 
-string username = "";
-string password = "";
+// string username = "";
+// string password = "";
 
 
-while (!(username == "Kalleanka" && password == "12345"))
+// while (!(username == "Kalleanka" && password == "12345"))
+// {
+
+//     username = Console.ReadLine();
+//     password = Console.ReadLine();
+
+//     if (username == "Kalleanka" && password == "12345")
+//     {
+//         Console.WriteLine("welcome");
+//     }
+
+//     else
+//     {
+//         Console.WriteLine("Wrong Username or pasword");
+//     }
+
+
+// }
+
+
+// int antal = 0;
+// while (antal <= 32)
+// {
+//     Console.WriteLine("Hello world");
+//     antal++;
+// }
+// Console.ReadLine();
+
+
+// int ja = 0;
+
+// while (ja <= 5)
+// {
+// string tal = Console.ReadLine();
+// int i = int.Parse(tal);
+
+// if (i > 5)
+// {
+// Console.WriteLine("Högre en 5");
+// }
+
+// ja++;
+// }
+
+
+// while(!int.TryParse(Console.ReadLine(),out int result));
+
+
+
+int r = 593;
+string g = "";
+int Gissa = 0;
+
+while(!(Gissa == 593))
 {
 
-    username = Console.ReadLine();
-    password = Console.ReadLine();
+Console.WriteLine("Gissa numret din manga");
 
-    if (username == "Kalleanka" && password == "12345")
-    {
-        Console.WriteLine("welcome");
-    }
+g = Console.ReadLine();
+Gissa = int.Parse(g);
 
-    else
-    {
-        Console.WriteLine("Wrong Username or pasword");
-    }
+if (Gissa == r)
+{
+ Console.WriteLine("Du gissade rätt");
+}
 
+if (Gissa > r)
+{
+    Console.WriteLine("För högt");
+}
 
+if (Gissa < r)
+{
+    Console.WriteLine("För Lågt");
 }
 
 
-int antal = 0;
-while (antal <= 32)
-{
-    Console.WriteLine("Hello world");
-    antal++;
+
+
+
 }
 Console.ReadLine();
-
-
-int ja = 0;
-
-while (ja <= 5)
-{
-string tal = Console.ReadLine();
-int i = int.Parse(tal);
-
-if (i > 5)
-{
-Console.WriteLine("Högre en 5");
-}
-
-ja++;
-}
-
-
-while(!int.TryParse(Console.ReadLine(),out int result));
-
-
-
-while( )
-{
-
-}
