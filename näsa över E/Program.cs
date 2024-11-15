4c0501f574667a032b25d9ae752b3cd995704afb
David Öjebrink Olssson <David.ojebrinkolsson@elev.ga.ntig.se>
2024-11-15 13:10:36 +0100

    fefef

--- näsa över E/Program.cs
+++ näsa över E/Program.cs
@@ -5,15 +5,61 @@
 // Console.WriteLine("Hello World");
 // }
 
-using System.Reflection.Metadata;
 
 string username = "";
 string password = "";
 
 
-while(!(username == "Kalleanka" && password == "12345"));
+while (!(username == "Kalleanka" && password == "12345"))
 {
- 
-    username = Console.ReadLine
+
+    username = Console.ReadLine();
+    password = Console.ReadLine();
+
+    if (username == "Kalleanka" && password == "12345")
+    {
+        Console.WriteLine("welcome");
+    }
+
+    else
+    {
+        Console.WriteLine("Wrong Username or pasword");
+    }
+
+
+}
+
+
+int antal = 0;
+while (antal <= 32)
+{
+    Console.WriteLine("Hello world");
+    antal++;
+}
+Console.ReadLine();
+
+
+int ja = 0;
+
+while (ja <= 5)
+{
+string tal = Console.ReadLine();
+int i = int.Parse(tal);
+
+if (i > 5)
+{
+Console.WriteLine("Högre en 5");
+}
+
+ja++;
+}
+
+
+while(!int.TryParse(Console.ReadLine(),out int result));
+
+
+
+while( )
+{
 
 }
